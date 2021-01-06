20e4bf8f423a5b9d9660ccb00262ed3787848e7c
Smith <77020542+Special-Esmit@users.noreply.github.com>
2021-01-06 16:31:02 +0000

    Add files via upload

---
+++ sn_target.py
@@ -0,0 +1,27 @@
+import sys,shodan
+
+print("<$> Creted By Special_Esmit")
+
+api_key = "your api_key"
+api = shodan.Shodan(api_key)
+
+if len(sys.argv) < 2:
+	target_ip = str(input("<^> Enter Target IP: "))
+else:
+	target_ip = sys.argv[1]
+
+try:
+	data = api.host(target_ip)
+	print("<^> Request Sent !\n\n\n")
+	print("<^> Your Target: " + str(data["ip_str"]) + "\n<^>Ports: " + str(data["ports"]) + "\n<^>Country Name: " + str(data["country_name"]) + "\n<^>Country Code: " + str(data["country_code"]) + "\n<^> City: " + str(data["city"]) + "\n<^>Postal Code: " + str(data["postal_code"]) + "\n<^>ORG: " + str(data["org"]) + "\n<^>Area Code: " + str(data["area_code"]) + "\n<^>ISP: " + str(data["isp"]) + "\n<^>Domains: " + str(data["domains"]) + "\n<^>Hostnames: " + str(data["hostnames"]) + "\n<^>ASN: " + str(data["asn"]) +"\n<^>os: " + str(data["os"]))
+	try:
+		print("\n<^>Vulns: " + str(data["vulns"]))
+	except:
+		print("<^> Target is Good :)")
+	x = str(input("Show Datas?(y,n): "))
+	if x == 'y':
+		print(data["data"])
+	else:
+		print("<^> by by")	
+except:
+	print("<!> Error ")
